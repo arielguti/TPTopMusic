edbb17fa0f80f1ae71db30f8ad6d15d6e78d0af4
arielg <arielg@192.168.0.15>
2022-05-26 21:32:11 -0300

    Creación de proyecto TPTopMusic

---
+++ TPTopMusic/src/main/java/ar/edu/unju/fi/topmusic/TpTopMusicApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.topmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpTopMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpTopMusicApplication.class, args);
+	}
+
+}
